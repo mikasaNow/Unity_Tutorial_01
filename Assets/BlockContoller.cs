c8be6932f80a11d57b041d4ff9591571481d21cf
mikasa <mikasanow@gmail.com>
2015-04-14 14:59:35 +0900

    step2_CreateRule

---
+++ Assets/BlockContoller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockContoller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Blockにボールが振れた際にBlockを削除
+	void OnCollisionEnter(Collision col) {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/BottomWallController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomWallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// 下壁にボールが振れた際にボールを削除
+	void OnCollisionEnter(Collision col) {
+		Destroy (col.gameObject);
+	}
+}
